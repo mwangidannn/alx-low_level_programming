2ea57b68c6d7b237ab8ecbea98854c1702b9ff48
mwangidannn <duncanmwangikinuthia@gmail.com>
2023-07-03 18:37:47 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+  *_putchar-write the character c to stdout
+  *oc:The  character to print
+  *
+  *Return:On success
+  *on error,-1 is returned,and errno is set appropritely
+  */
+int main(char c)
+{
+
+	return(write(1,&c,);)
+}
