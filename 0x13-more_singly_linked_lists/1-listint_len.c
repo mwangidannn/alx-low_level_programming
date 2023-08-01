834712c743d18324b4b4d6591f0e6da78bd3d9b8
mwangidannn <duncanmwangikinuthia@gmail.com>
2023-07-31 19:45:37 -0700

    list length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,17 +1,22 @@
-#include "list.h"
+#include "lists.h"
+#include <stdio.h>
+
 /**
  *functn listint_len return entire number of elements in a linked lists
  * head  linked list of type listint_t to traverse
  *
  * will  Return: number of nodes
  */
+
 size_t listint_len(const listint_t *h)
 {
-size_t numb = 0;
-while (head != NULL)
-{
-numb++;
-h = h->next;
-}
-return (numb);
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,21 +1,21 @@
-#ifndef LIST_H
-#define LIST_H
-
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
 
-**
- *  struct listint_s -which is a  singly linked list
- * @n: is a integer
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
  *
- * Description: the singly linked list node structure
- *
+ * Description: singly linked list node structure
+ * for project
  */
+
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -33,4 +33,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
 #endif
