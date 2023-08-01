f5953f530bcab6cb0566d59b612467226ed927bb
mwangidannn <duncanmwangikinuthia@gmail.com>
2023-07-31 18:58:53 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,19 @@
-#include "lists.h"
+#include <stdio.h>
+#include  "list.h"
 /**
- * The print_listint - it will display the entire  elements of a linked list
- * the header will  linked list of type listint_t to display
+ * the  print_listint -will  prints all the elements of a linked list
+ * the header lisistint_t to print
  *
- * Return: number of nodes
+ * then Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-size_t count = 0
+size_t to_nodes = 0;
 while (h != NULL)
 {
-printf("%d\n", h->data);
-count++;
+printf("%d\n", h->n);
+to_nodes++;
 h = h->next;
 }
-return count;
+return (to_nodes);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,15 +1,15 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <stdio.h>
+
 #include <stdlib.h>
 
 **
- * struct listint_s - singly linked list
- * @n: integer
+ *  struct listint_s -which is a  singly linked list
+ * @n: is a integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description: the singly linked list node structure
  *
  */
 typedef struct listint_s
@@ -17,6 +17,7 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
