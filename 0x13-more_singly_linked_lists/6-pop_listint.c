e60e295410be2c12332dbaf4cc1aec40e64c1c2b
mwangidannn <duncanmwangikinuthia@gmail.com>
2023-08-01 12:31:27 -0700

    Get node at index

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,25 +1,28 @@
 #include "lists.h"
 /**
- * pop_listint to deletes the head node of a linked list
- * @h points to the first element in the linked list
- *
+ * pop_listint - deletes the head node of a linked list
+ * @head: pointer to the first element in the linked list
  * Return: data inside elements that was deleted
  * 0 when thee list is empty
  */
-int pop_listint(listint_t **head);
+
+int pop_listint(listint_t **head)
 {
 if (head == NULL || *head == NULL)
 {
 return (0);
 }
-int data = (*head)->n;
-listint_t *temp = *head;
+
+data =(*head)->n;
+listint_t *temp = head;
 *head = (*head)->next;
 free(temp);
 return (data);
 }
-void print_list(listint_t **head);
+
+void print_list(listint_t *head)
 {
+
 listint_t *current_node = head;
 while (current_node != NULL)
 {
