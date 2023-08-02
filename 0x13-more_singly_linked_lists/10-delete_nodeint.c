2c8ebd27d86f2b3537e35474c096d532b7f42ef3
mwangidannn <duncanmwangikinuthia@gmail.com>
2023-08-01 18:56:53 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,34 +7,45 @@
  *
  * Return: 1 (Success), or -1 (Fail)
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index) {
-if (head == NULL || *head == NULL) 
-return -1; // The linked list is empty, return -1 (failure)
-if (index == 0) 
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+if (head == NULL || *head == NULL)
+return (-1);
+if (index == 0)
 {
 listint_t *temp = *head;
 *head = (*head)->next;
 free(temp);
-return 1;
+return (1);
 }
+
 listint_t *current_node = *head;
 unsigned int current_index = 0;
-while (current_node->next != NULL && current_index < index - 1) {
+while (current_node->next != NULL && current_index < index - 1)
+{
 current_node = current_node->next;
 current_index++;
 }
 
-if (current_node->next == NULL || current_index < index - 1) {
-return -1
+if (current_node->next == NULL || current_index < index - 1)
+{
+
+return (-1);
+
 }
-listint_t *node_to_delete = current_node->next;
+
+listint_t *node_to_delete =current_node->next;
 current_node->next = node_to_delete->next;
 free(node_to_delete);
-return 1;
+return (1);
 }
-void print_list(listint_t *head) {
+
+void print_list(listint_t *head);
+{
 listint_t *current_node = head;
-while (current_node != NULL) { printf("%d -> ", current_node->n);
+while (current_node != NULL)
+{
+printf("%d -> ", current_node->n);
 current_node = current_node->next;
 }
 printf("NULL\n");
