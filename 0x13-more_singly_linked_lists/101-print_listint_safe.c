22ebe3d016238ee870449c2ac26161c4ad3d4d4a
mwangidannn <duncanmwangikinuthia@gmail.com>
2023-08-02 01:35:46 -0700

    Pop

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,16 +1,13 @@
-#include "lists.h"
+i#include "lists.h"
 #include <stdio.h>
-
 size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 
 /**
- * looped_listint_len - Counts the number of unique nodes
- * in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
- *
+ * looped_listint_len - add the number of different nodes in a list
+ * @head: A points to the head of the listint_t to check.
  * Return: If the list is not looped - 0.
- * Otherwise - the number of unique nodes in the list.
+ * else  - the number of different nodes in the list.
  */
 size_t looped_listint_len(const listint_t *head)
 {
@@ -44,7 +41,6 @@ hare = (hare->next)->next;
 }
 return (0);
 }
-
 /**
  * print_listint_safe - Prints a listint_t list safely.
  * @head: A pointer to the head of the listint_t list.
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,33 +1,20 @@
 #include "lists.h"
 /**
- * pop_listint - deletes the head node of a linked list
- * @head: pointer to the first element in the linked list
+ * pop_listint - deletes the first node of  list
+ * @head: points to the first element in the linked list
  * Return: data inside elements that was deleted
- * 0 when thee list is empty
+ * 0 when list was  empty
  */
-
 int pop_listint(listint_t **head)
 {
-if (head == NULL || *head == NULL)
-{
+listint_t *few;
+int count;
+if (!head || !*head)
 return (0);
+count = (*head)->n;
+few = (*head)->next;
+free(*head);
+*head = few;
+return (count);
 }
 
-data =(*head)->n;
-listint_t *temp = head;
-*head = (*head)->next;
-free(temp);
-return (data);
-}
-
-void print_list(listint_t *head)
-{
-
-listint_t *current_node = head;
-while (current_node != NULL)
-{
-printf("%d -> ", current_node->n);
-current_node = current_node->next;
-}
-printf("NULL\n");
-}
