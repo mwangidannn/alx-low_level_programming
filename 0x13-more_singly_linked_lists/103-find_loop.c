244018234cd38eb0427ce4d5bde5754c831a6ccc
mwangidannn <duncanmwangikinuthia@gmail.com>
2023-08-01 23:47:21 -0700

    findthe loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,66 +1,31 @@
 #include "lists.h"
 /**
- * find_listint_loop - finds the loop in a linked list
+ * find_listint_loop - trace the loop  linked list
  * @head: linked list to search for
  *
- * Return: address of the node where the loop starts, or NULL
- */
+ * Return: address of the node where the loops strats annd and null
+*/
 listint_t *find_listint_loop(listint_t *head)
 {
-listint_t *slow = head;
 listint_t *fast = head;
-while (fast != NULL && fast->next != NULL)
+listint_t *slow = head;
+if (!head)
+return (NULL);
+while (slow && fast && fast->next)
 {
-slow = slow->next;
 fast = fast->next->next;
-if (slow == fast)
+slow = slow->next;
+if (fast == slow)
 {
 slow = head;
 while (slow != fast)
 {
 slow = slow->next;
-
 fast = fast->next;
-{
-return (slow);
+}
+return (fast);
 }
 }
 return (NULL);
 }
-void create_loop(listint_t *head, int loop_index)
-{
-listint_t *loop_start = head;
 
-while (loop_index > 0)
-
-{
-loop_start = loop_start->next
-loop_index--;
-}
-listint_t *current_node = head;
-while (current_node->next != NULL)
-{
-current_node = current_node->next;
-}
-current_node->next = loop_start
-}
-void print_list(listint_t *head);
-{
-listint_t *current_node = head;
-int count = 0;
-while (current_node != NULL && count < 15)
-{
-printf("%d -> ", current_node->n);
-current_node = current_node->next;
-count++;
-}
-if (current_node != NULL)
-{
-printf("... (loop detected)\n");
-}
-else
-{
-printf("NULL\n");
-}
-}
-}
