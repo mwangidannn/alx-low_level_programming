3dc56582022d95e3a57ddf4c0bae2631bfef58a5
mwangidannn <duncanmwangikinuthia@gmail.com>
2023-08-01 19:54:26 -0700

    reverse linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,52 +1,33 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes a node in a linked list at a certain index
- * @head: pointer to the first element in the list
- * @index: index of the node to delete
- *
- * Return: 1 (Success), or -1 (Fail)
+ * delete_nodeint_at_index - remove node in  linked list any dex
+ * head: pointer to the first element in the list
+ * index of  node to remove
+ * Return: 1 (Success), or -1 for error mgmt
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-if (head == NULL || *head == NULL)
+istint_t *temp = *head;
+listint_t *current = NULL;
+unsigned int i = 0;
+if (*head == NULL)
 return (-1);
 if (index == 0)
 {
-listint_t *temp = *head;
 *head = (*head)->next;
 free(temp);
 return (1);
 }
-
-listint_t *current_node = *head;
-unsigned int current_index = 0;
-while (current_node->next != NULL && current_index < index - 1)
+while (i < index - 1)
 {
-current_node = current_node->next;
-current_index++;
-}
-
-if (current_node->next == NULL || current_index < index - 1)
-{
-
+if (!temp || !(temp->next))
 return (-1);
-
+temp = temp->next;
+i++;
 }
-
-listint_t *node_to_delete =current_node->next;
-current_node->next = node_to_delete->next;
-free(node_to_delete);
+current = temp->next;
+temp->next = current->next;
+free(current);
 return (1);
-}
-
-void print_list(listint_t *head);
-{
-listint_t *current_node = head;
-while (current_node != NULL)
-{
-printf("%d -> ", current_node->n);
-current_node = current_node->next;
-}
-printf("NULL\n");
 }
