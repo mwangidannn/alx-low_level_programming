1c3a1aeeadc6e01515c2e0990a640af86235561f
mwangidannn <duncanmwangikinuthia@gmail.com>
2023-08-02 07:44:25 -0700

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,73 +1,32 @@
-i#include "lists.h"
+#include "lists.h"
 #include <stdio.h>
-size_t looped_listint_len(const listint_t *head);
-size_t print_listint_safe(const listint_t *head);
-
+#include <stdlib.h>
 /**
- * looped_listint_len - add the number of different nodes in a list
- * @head: A points to the head of the listint_t to check.
- * Return: If the list is not looped - 0.
- * else  - the number of different nodes in the list.
- */
-size_t looped_listint_len(const listint_t *head)
-{
-const listint_t *tortoise, *hare;
-size_t nodes = 1;
-if (head == NULL || head->next == NULL)
-return (0);
-tortoise = head->next;
-hare = (head->next)->next;
-while (hare)
-{
-if (tortoise == hare)
-{
-tortoise = head;
-while (tortoise != hare)
-{
-nodes++;
-tortoise = tortoise->next;
-hare = hare->next;
-}
-tortoise = tortoise->next;
-while (tortoise != hare)
-{
-nodes++;
-tortoise = tortoise->next;
-}
-return (nodes);
-}
-tortoise = tortoise->next;
-hare = (hare->next)->next;
-}
-return (0);
-}
-/**
- * print_listint_safe - Prints a listint_t list safely.
- * @head: A pointer to the head of the listint_t list.
+ * print_listint_safe - Prints a listint_t linked list safely (handles loops)
+ * @head: Pointer to the head node of the linked list
  *
- * Return: The number of nodes in the list.
+ * Return: The number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-size_t nodes, index = 0;
-nodes = looped_listint_len(head);
-if (nodes == 0)
+size_t count = 0;
+const listint_t *current = head;
+while (current != NULL)
 {
-for (; head != NULL; nodes++)
+printf("%d", current->n);
+count++;
+if (current->next == NULL)
 {
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
+printf("\n");
+return (count);
 }
-}
-else
+printf(" -> ");
+if (current->next >= current)
 {
-for (index = 0; index < nodes; index++)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
+printf("... (loop detected\n");
+exit(98);
 }
-printf("-> [%p] %d\n", (void *)head, head->n);
+current = current->next;
 }
-return (nodes);
+return count;
 }
-
