74e1ba26fe1500aec44ed98e3da4f4c28eae6ef2
mwangidannn <duncanmwangikinuthia@gmail.com>
2023-09-11 12:18:25 -0700

    new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - a dog's basic info
  * @name: Number First object
@@ -27,5 +26,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
-
-
+#endif
